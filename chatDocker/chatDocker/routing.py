a5789d51cbc08e2239ea2ccd0ec6a24aa86cffa3
ailan <ailan@ailan-X556UQ>
2018-12-04 19:39:25 +0800

    'hosts': [('redis', 6379)] ??????

--- chatDocker/chatDocker/routing.py
+++ chatDocker/chatDocker/routing.py
@@ -1,6 +1,3 @@
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
